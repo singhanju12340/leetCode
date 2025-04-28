be7610982de6a2360c947a6b04601c3e84ba5319
Anju <singhanju12340@gmail.com>
2025-04-28 17:32:32 +0530

    Time: 2 ms (99.97%), Space: 54.8 MB (7.46%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.java
+++ 1039-find-the-town-judge/1039-find-the-town-judge.java
@@ -1,4 +1,24 @@
 class Solution {
+
+// best solution
+// O(N) N space 
+     public int findJudge(int n, int[][] trust) {
+        int[] pairArray = new int[n+1];
+        
+
+           for(int t[] : trust){
+            pairArray[t[0]]--;     // outdegree
+            pairArray[t[1]]++;   // indegree
+        }
+        
+        for(int i=1;i<=n;i++){
+            if(pairArray[i]==n-1) return i;
+        }
+        
+        return -1;
+    }
+
+
     public int findJudge2(int n, int[][] trust) {
         int[] people = new int[1000];
         for(int i=0;i<trust.length;i++){
@@ -11,7 +31,8 @@ class Solution {
         return -1;
     }
 
-    public int findJudge(int n, int[][] trust) {
+// hashmap based solution, O(N) + space O(2N)
+    public int findJudge3(int n, int[][] trust) {
         int[] people = new int[n];
         Map<Integer, Set<Integer>> map = new HashMap<>();
       if(trust.length==0 && n==1)
