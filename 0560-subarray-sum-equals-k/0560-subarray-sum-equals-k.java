aafaf9c34877b36f0e227b6dd389a81be3539f84
Anju <singhanju12340@gmail.com>
2025-04-29 11:56:34 +0530

    Time: 24 ms (75.08%), Space: 46.9 MB (26.43%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,5 +1,7 @@
 class Solution {
-    public int subarraySum(int[] nums, int k) {
+
+    // n2 + extra space solution, too bad 1500ms 
+    public int subarraySum2(int[] nums, int k) {
         int result=0;
         int[] prefixSum = new int[nums.length];
         prefixSum[0] = nums[0];
@@ -15,6 +17,26 @@ class Solution {
                 if( prefixSum[j]-prefixSum[i] == k)
                 result++;
             }
+        }
+        return result;
+    }
+
+
+
+   public int subarraySum(int[] nums, int k) {
+        // it will contain difference sum as key and its frequency as value
+        // as we are calculating prefix sum, so we know all the existence of sum till current index.
+        Map<Integer, Integer> map = new HashMap<>();
+        int currentSum=0;
+        int result =0;
+        map.put(0,1); // if current num=k. it should be taken as a single subarray
+
+        for(int i=0;i<nums.length;i++){
+            currentSum+=nums[i];
+           if(map.containsKey(currentSum-k)){
+               result += map.get(currentSum-k);
+           }
+           map.put(currentSum, map.getOrDefault(currentSum,0)+1);
         }
         return result;
     }
