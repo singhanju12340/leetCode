ae083b7632f27dc7b7c935a8691a15d919e54706
Anju <singhanju12340@gmail.com>
2025-04-29 15:38:37 +0530

    Time: 30 ms (12.74%), Space: 62.2 MB (22.89%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int currentSum=0;
+        map.put(0,-1); // for example:[23,2,4,6,6]
+
+
+        for(int i=0;i<nums.length;i++){
+            currentSum+=nums[i];
+
+// if there is any remainder already present, means the values between these 2 indexes are multiple of k 
+// ex: 23,2,4,6:  on 23th value remainder is 5. after adding 2+4= to 23, again remainder is 5( 29%6=5)
+// it means added value from 23 to current is divisible of k
+
+            if(map.containsKey(currentSum%k) && map.get(currentSum%k)<=i-2){
+                return true;
+            }
+            
+            if(map.containsKey(currentSum%k)){
+                continue;
+            }else{
+                map.put(currentSum%k,i);
+            }
+        }
+
+        return false;
+    }
+}
