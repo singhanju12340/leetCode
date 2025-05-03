4c65d2344d7ab05165d1b80930a831b649333711
Anju <singhanju12340@gmail.com>
2025-05-03 22:19:48 +0530

    Time: 18 ms (97.75%), Space: 83.6 MB (56.16%) - LeetHub

--- 2035-count-sub-islands/2035-count-sub-islands.java
+++ 2035-count-sub-islands/2035-count-sub-islands.java
@@ -10,13 +10,12 @@ class Solution {
     public boolean isValid=true;
     public int countSubIslands(int[][] grid1, int[][] grid2) {
         int count = 0;
-        int[][] visited=new int[grid1.length][grid1[0].length];
         for(int i=0;i<grid2.length;i++){
 
             for(int j=0;j<grid2[0].length;j++){
 
-                if(grid2[i][j] == 1 && visited[i][j]==0){
-                    dfs(grid1, grid2, i, j, visited);
+                if(grid2[i][j] == 1){
+                    dfs(grid1, grid2, i, j);
                     if(isValid) // if its a subarray
                         count++;
                     isValid = true;    
@@ -28,20 +27,18 @@ class Solution {
     }
 
 
-
-
-    public void dfs(int[][] grid1,int[][] grid2, int x, int y, int[][] visited){
+    public void dfs(int[][] grid1,int[][] grid2, int x, int y){
         
         if(x>=0 && y>=0 && x<grid2.length && y<grid2[0].length 
-        && grid2[x][y] == 1 && visited[x][y]==0)
+        && grid2[x][y] == 1)
         {
             if(grid1[x][y] == 0)
                 isValid=false;
-            visited[x][y] = 1;
-            dfs(grid1, grid2, x, y+1, visited);
-            dfs(grid1, grid2, x+1, y, visited);
-            dfs(grid1, grid2, x, y-1, visited);
-            dfs(grid1, grid2, x-1, y, visited);
+            grid2[x][y] = 0;
+            dfs(grid1, grid2, x, y+1);
+            dfs(grid1, grid2, x+1, y);
+            dfs(grid1, grid2, x, y-1);
+            dfs(grid1, grid2, x-1, y);
         }
     }
 }
